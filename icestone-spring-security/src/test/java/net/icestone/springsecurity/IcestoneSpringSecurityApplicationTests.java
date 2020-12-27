3fb9630207dacb71939c3d4609f310f87d14f2f6
dougl <dougl@LAPTOP-S45BDKLO>
2020-12-27 13:17:32 +0800

    Add Junit Test

--- icestone-spring-security/src/test/java/net/icestone/springsecurity/IcestoneSpringSecurityApplicationTests.java
+++ icestone-spring-security/src/test/java/net/icestone/springsecurity/IcestoneSpringSecurityApplicationTests.java
@@ -1,13 +1,22 @@
 package net.icestone.springsecurity;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import net.icestone.springsecurity.controller.LoginController;
 
 @SpringBootTest
 class IcestoneSpringSecurityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private LoginController controller;
 
+	@Test
+	void contextLoads()throws Exception {
+	    assertThat(controller).isNotNull();
+	     }
+	
 }
