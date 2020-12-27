777f6cdfbf3b1df507e554b4426afe1cd768b96e
dougl <dougl@LAPTOP-S45BDKLO>
2020-12-27 11:24:24 +0800

    First commit

---
+++ icestone-spring-security/src/main/java/net/icestone/springsecurity/IcestoneSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package net.icestone.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcestoneSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcestoneSpringSecurityApplication.class, args);
+	}
+
+}
